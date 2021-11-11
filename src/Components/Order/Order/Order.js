89a2909dbc37983cf2e89edd922255c6585d1be6
unknown <555REDOY555>
2021-11-11 23:46:48 +0600

    all good

--- src/Components/Order/Order/Order.js
+++ src/Components/Order/Order/Order.js
@@ -1,5 +1,6 @@
 import Button from '@restart/ui/esm/Button';
 import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import OrderMordal from '../OrderMordal/OrderMordal';
@@ -27,35 +28,37 @@ const Order = () => {
      return (
 
           <div>
-               <div className="row">
-                    <div className="border border-4">
-                         <div className="card     ">
-                              <img src={booking.img} className="card-img-top img-fluid  " alt="..." />
-                              <div className="card-body">
-                                   <h5 className="card-title">{booking.name}</h5>
-                                   <p className="card-text text-primary ">{booking.description}</p>
-                                   <h2 className=" mb-3 " >
-                                        Price: {booking.price}
-                                   </h2>
-                                   <h2 className=" mb-3 " >
-                                        Top-Speed :  {booking.TopSpeed} MPH
-                                   </h2>
-                                   <h2 className=" mb-3 " >
-                                        Fuel:  {booking.Fuel}
-                                   </h2>
-                                   <h2 className=" mb-3 " >
-                                        Body-Type:  {booking.BodyType}
-                                   </h2>
-                                   <Link to={`/OrderMordal/${booking._id}`} >
+               <Container className='my-5'>
+                    <div className="row">
+                         <div className="border border-4">
+                              <div className="card     ">
+                                   <img src={booking.img} className="card-img-top img-fluid  " alt="..." />
+                                   <div className="card-body">
+                                        <h5 className="card-title">{booking.name}</h5>
+                                        <p className="card-text text-primary ">{booking.description}</p>
+                                        <h2 className=" mb-3  " >
+                                             <span className='text-success' >Price</span> : <span className='text-secondary' >{booking.price}</span>
+                                        </h2>
+                                        <h2 className=" mb-3  " >
+                                             <span className='text-success' >Top-Speed</span> :  <span className='text-secondary' >{booking.TopSpeed}</span> MPH
+                                        </h2>
+                                        <h2 className=" mb-3 " >
+                                             <span className='text-success' >Fuel</span>:  <span className='text-secondary' >{booking.Fuel}</span>
+                                        </h2>
+                                        <h2 className=" mb-3 " >
+                                             <span className='text-success' >Body-Type</span>:  <span className='text-secondary' >{booking.BodyType}</span>
+                                        </h2>
+                                        <Link to={`/OrderMordal/${booking._id}`} >
 
-                                        <Button className="btn btn-warning" >
-                                             Order now
-                                        </Button>
-                                   </Link>
+                                             <Button className="btn btn-warning" >
+                                                  Order now
+                                             </Button>
+                                        </Link>
+                                   </div>
                               </div>
                          </div>
                     </div>
-               </div>
+               </Container>
           </div>);
 };
 
