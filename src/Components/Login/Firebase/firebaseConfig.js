4c2e56f832538148aeb03e1296fe78092bfaba1e
REDOY <74199587+555REDOY555@users.noreply.github.com>
2021-11-26 21:38:32 +0600

    go

--- src/Components/Login/Firebase/firebaseConfig.js
+++ src/Components/Login/Firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-     apiKey: "AIzaSyCWSwg5gGM8gWD-DMH4oLLWuUmZa2akrvg",
-  authDomain: "cars-a30a7.firebaseapp.com",
-  projectId: "cars-a30a7",
-  storageBucket: "cars-a30a7.appspot.com",
-  messagingSenderId: "840094560225",
-  appId: "1:840094560225:web:69307d5defc09c7f40aca0"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebaseConfig;
